5b1932f6d72aba47b4a74dfb213c496a940a06f1
Volodymyr Budnyi <volodymyr.budnyi@eleks.com>
2019-05-16 11:31:40 +0300

    Add swagger to project

---
+++ TechnicalAsignment/App_Start/SwaggerConfig.cs
@@ -0,0 +1,24 @@
+using System.Web.Http;
+using WebActivatorEx;
+using TechnicalAsignment;
+using Swashbuckle.Application;
+
+[assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
+
+namespace TechnicalAsignment
+{
+    public class SwaggerConfig
+    {
+        public static void Register()
+        {
+            var thisAssembly = typeof(SwaggerConfig).Assembly;
+
+            GlobalConfiguration.Configuration
+                .EnableSwagger(c =>
+                    {                       
+                        c.SingleApiVersion("v1", "TechnicalAsignment");                        
+                    })
+                .EnableSwaggerUi();
+        }
+    }
+}
